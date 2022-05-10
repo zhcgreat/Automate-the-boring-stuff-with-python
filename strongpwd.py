814b9f6f25c1113ea09211e74e3e4afa0ed0e2d1
zhcgreat <75013824+zhcgreat@users.noreply.github.com>
2022-05-10 22:20:06 +0800

    Create strongpwd.py

---
+++ strongpwd.py
@@ -0,0 +1,18 @@
+import re
+checkuppercase = re.compile(r'[A-Z]')
+checklowercase = re.compile(r'[a-z]')
+checkdigit = re.compile(r'\d')
+checklens = re.compile(r'.{8,}')
+elif checklens.search(pwd) == None:
+print ('Your password should be at least eight characters long.')
+else:
+print ('Your password is strong.')
+def strongpwdcheck(pwd):
+    if checkuppercase.search(pwd) == None:
+        print ('Your password should contain upper case.')
+    elif checklowercase.search(pwd) == None:
+        print ('Your password should contain lower case.')
+    elif checklens.search(pwd) == None:
+        print ('Your password should be at least eight characters long.')
+    else:
+        print ('Your password is strong.')
